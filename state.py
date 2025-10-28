aad6c4f25c52f6fe00b1b4993a7f5c29a72409a6
KappuChinooo <KappuChinooo@gmail.com>
2025-10-28 20:49:48 +0700

    fix stupid shit

--- state.py
+++ state.py
@@ -8,10 +8,10 @@ class ChannelStateManager:
 
     async def start_session(self, channel_id, owner_id):
         async with self._lock:
-            if(channel_id in self.active_session):
+            if(channel_id in self.active_sessiosn):
                 raise ValueError("Channel already has an active game.")
             game = GuessSession(owner_id)
-            self.active_session[channel_id] = game
+            self.active_sessions[channel_id] = game
             return game
         
     async def end_session(self, channel_id):
